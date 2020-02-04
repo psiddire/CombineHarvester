c9d6935ee4d6604d756fb53637aca20a38f76afb
Prasanna Siddireddy <psiddire@nd.edu>
2020-02-04 23:46:46 +0100

    MuTau BDT 2017

---
+++ CombineTools/MuTau/2017/shapes_mt_BDT/Limits.sh
@@ -0,0 +1,6 @@
+combineCards.py Name1=HMuTau_mutauhad_1_2017_m125.txt Name2=HMuTau_mutauhad_2_2017_m125.txt Name3=HMuTau_mutauhad_3_2017_m125.txt Name4=HMuTau_mutauhad_4_2017_m125.txt > HMuTau_mutauhad_comb_2017_m125.txt
+combine -M AsymptoticLimits -m 125 HMuTau_mutauhad_1_2017_m125.txt --run blind
+combine -M AsymptoticLimits -m 125 HMuTau_mutauhad_2_2017_m125.txt --run blind
+combine -M AsymptoticLimits -m 125 HMuTau_mutauhad_3_2017_m125.txt --run blind
+combine -M AsymptoticLimits -m 125 HMuTau_mutauhad_4_2017_m125.txt --run blind
+combine -M AsymptoticLimits -m 125 HMuTau_mutauhad_comb_2017_m125.txt --run blind
